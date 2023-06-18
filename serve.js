a50d5795a459aa1ec28abc099ced97678299bc54
xdadev37 <pouriasa37@gmail.com>
2023-06-18 18:35:31 +0330

    script fixed

--- serve.js
+++ serve.js
@@ -1,10 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
-var path_1 = require("path");
 var app = (0, express_1)();
-app.use(express_1.static(path_1.join(__dirname, '/srv')));
-app.get('/*', function (req, res) {
-    res.sendFile(path_1.join(__dirname, '/srv', 'index.html'));
+app.use(express_1.static("/srv"));
+app.get("/*", function (req, res) {
+    res.sendFile("/srv/index.html");
 });
-app.listen(3000, function () { return console.log('serving ...'); });
+app.listen(3000, function () { return console.log("serving ..."); });
--- serve.ts
+++ serve.ts
@@ -1,10 +1,9 @@
 import express from "express";
-import path from "path";
 
 const app = express();
-app.use(express.static(path.join(__dirname, "/srv")));
+app.use(express.static("/srv"));
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/srv", "index.html"));
+  res.sendFile("/srv/index.html");
 });
 
-app.listen(3000, () => console.log("served"));
+app.listen(3000, () => console.log("serving ..."));
